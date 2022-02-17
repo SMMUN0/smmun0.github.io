937767f4618b57c807a0a53c5a8bc127fb6a61e7
Alonso <alonso12_284@outlook.com>
2022-02-16 20:11:00 -0600

    comite

--- script/committees.js
+++ script/committees.js
@@ -6,19 +6,12 @@ function addElement () {
     })
     .then(response => {
         var committees = response;
+        var currentDiv = document.getElementById("committeesList");
 
-        var currentDiv = document.getElementById("committees");
+        for(var i = 0; i < committees.length-1; i++){
 
-        console.log("HERE");
-
-        for(var i = 0; i < committees.length; i+=2){
-
-            var newDiv = document.createElement("div");
-            newDiv.classList.add("group");
-            if(i+1 == committees.length){
-                // single committee
-                var middleDiv = document.createElement("div");
-                middleDiv.classList.add("committee-middle");
+            // Single committee
+                var middleDiv = document.createElement("li");
 
                 var img = document.createElement("img");
                 img.src = "./style/img/committees/" + committees[i]["CODE"] + ".png";
@@ -29,53 +22,32 @@ function addElement () {
                 link.appendChild(img);
 
                 var header = document.createElement("h1");
-                header.innerHTML = committees[i]["NAME"];
+                header.innerHTML = committees[i]["CODE"];
                 header.className = "committee";
 
                 middleDiv.appendChild(link);
                 middleDiv.appendChild(header);
-                newDiv.appendChild(middleDiv);
-            }else{
-                // double committe
-                var leftDiv = document.createElement("div");
-                leftDiv.classList.add("committee-left");
-                var rigthDiv = document.createElement("div");
-                rigthDiv.classList.add("committee-rigth");
-
-                var imgL = document.createElement("img");
-                var headerL = document.createElement("h1");
-                imgL.classList.add("committee")
-                imgL.src = "./style/img/committees/" + committees[i+1]["CODE"] + ".png";
-
-                var linkL = document.createElement("a");
-                linkL.href = "/committees/" + committees[i+1]["CODE"] + "/"+ committees[i+1]["CODE"] + ".html";
-                linkL.appendChild(imgL);
-
-                headerL.className = "committee";
-                headerL.innerHTML = committees[i+1]["NAME"];
-                leftDiv.appendChild(linkL);
-                leftDiv.appendChild(headerL);
-                newDiv.appendChild(leftDiv);
-
-                var imgR = document.createElement("img");
-                var headerR = document.createElement("h1");
-                imgR.classList.add("committee")
-                imgR.src = "./style/img/committees/" + committees[i]["CODE"] + ".png";
-
-                var linkR = document.createElement("a");
-                linkR.href = "/committees/" + committees[i]["CODE"] + "/" + committees[i]["CODE"] + ".html";
-                linkR.appendChild(imgR);
-
-                headerR.className = "committee";
-                headerR.innerHTML = committees[i]["NAME"];
-                rigthDiv.appendChild(linkR);
-                rigthDiv.appendChild(headerR);
-                newDiv.appendChild(rigthDiv);
-                
-            }
-
-            currentDiv.appendChild(newDiv);
+                currentDiv.appendChild(middleDiv);
         }
+
+        // Prensa committee
+        var Prensa = document.getElementById("Prensa");
+
+        var img = document.createElement("img");
+        img.src = "./style/img/committees/" + committees[committees.length-1]["CODE"] + ".png";
+        img.classList.add("committee")
+
+        var link = document.createElement("a");
+        link.href = "/committees/" + committees[committees.length-1]["CODE"] + "/"+ committees[i]["CODE"] + ".html";
+        link.appendChild(img);
+
+        var header = document.createElement("h1");
+        header.innerHTML = committees[committees.length-1]["CODE"];
+        header.className = "committee";
+
+        Prensa.appendChild(link);
+        Prensa.appendChild(header);
+
 
     })
 
