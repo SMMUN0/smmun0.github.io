dc861ea5cbd20eaf037527a8cb0fcc797eaea277
Alonso <alonso12_284@outlook.com>
2022-04-30 16:32:34 -0500

    secreatriat

--- script/about.js
+++ script/about.js
@@ -1,61 +1,8 @@
 function setUpImage() { 
-    carousel = document.getElementById("carouselExampleIndicators");
-    fetch("./secretariats/2022.json")
-    .then(response => {
-        return response.json();
-    })
-    .then(secretariat => {
-        sizeOfCarousel = Math.ceil(secretariat.length/4);
-        carouselIndicators = document.getElementById("carousel-indicators");
-        carouselItems = document.getElementById("carousel-inner");
+    list = document.getElementById("secreatariat");
 
-        for(var i = 0; i < sizeOfCarousel; i++){
-            var indicator = document.createElement("li");
-            indicator.dataset.target = "#carouselExampleIndicators";
-            indicator.setAttribute("data-slide-to","\""+i+"\"");
-            indicator.id = "indicator"+i;
-            carouselIndicators.appendChild(indicator);
+    for(var i = 1; i <= 14;i++){
+        list.innerHTML += '<li><img src="./secreatariats/' + i + '.jpg" alt=""></li>';
+    }
 
-            var item = document.createElement("div");
-            item.classList.add("carousel-item");
-            item.id = "item"+i;
-            var newGallery = document.createElement("div");
-            newGallery.classList.add("newGallery");
-            newGallery.id = "newGallery"+i;
-
-            item.appendChild(newGallery);
-            carouselItems.appendChild(item);
-        }
-
-        document.getElementById("indicator0").classList.add("active");
-        document.getElementById("item0").classList.add("active");
-        
-        var indx = 0;
-        secretariat.forEach(person => {
-            newGallery = document.getElementById("newGallery"+Math.floor(indx/4));
-
-            var gallery = document.createElement("div");
-            gallery.classList.add("gallery");
-
-            var a = document.createElement("a");
-            a.target = "_blank";
-            a.href = "./style/img/secretariats/2022/"+person["ID"]+".jpg";
-
-            var img = document.createElement("img");
-            img.src = "./style/img/secretariats/2022/"+person["ID"]+".jpg";
-            img.alt = person["Nombre"] + " " + person["Primer Apellido"];
-
-            var desc = document.createElement("div");
-            desc.classList.add("desc");
-            desc.innerHTML = "<h2>"+person["Nombre"]+" "+person["Primer Apellido"]+" "+person["Segundo Apellido"]+"</h2>"+"<h3>"+person["Secretaria"]+"<\h3>";
-
-
-            a.appendChild(img);
-            gallery.appendChild(a);
-            gallery.appendChild(desc);
-            newGallery.appendChild(gallery);
-
-            indx++;
-        });
-    });
 }
