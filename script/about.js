df34b549d8420c7130652b0e661c7b37fadf4851
Alonso <alonso12_284@outlook.com>
2022-10-12 10:29:31 -0500

    update

--- script/about.js
+++ script/about.js
@@ -1,9 +1,4 @@
 function setUpImage() { 
-    // list = document.getElementById("secreatariat");
-
-    // for(var i = 1; i <= 14;i++){
-    //     list.innerHTML += '<li><img src="./secreatariats/' + i + '.jpg" alt=""></li>';
-    // }
 
     fetch("../../models.json")
     .then(response => {
