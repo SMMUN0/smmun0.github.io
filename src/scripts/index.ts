543081f1f6a75996ece5a186c3f56101d5e709db
Bruno Ancona <brunoanconasala@gmail.com>
2024-05-09 09:26:06 -0600

    Modularity

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,43 @@
+// Update navbar after scrolling
+const nav = document.getElementById("navbar") as HTMLElement;
+const navMobile = document.getElementById("navbar-mobile") as HTMLElement;
+const navLogo = document.getElementsByClassName("navbar-logo")[0] as HTMLElement;
+const header = document.getElementById("greeting") as HTMLElement;
+const navHeight = nav.getBoundingClientRect().height;
+
+function updateNavColor(entries: IntersectionObserverEntry[]) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) {
+        nav.classList.add("nav-colored");
+        navMobile.classList.add("nav-colored");
+
+        navLogo.style.display = "inherit";
+        navLogo.style.opacity = "1";
+
+        nav.classList.remove("nav-transparent");
+        navMobile.classList.remove("nav-transparent");
+    }
+    else {
+        nav.classList.add("nav-transparent");
+        navMobile.classList.add("nav-transparent");
+
+        navLogo.style.display = "none";
+        navLogo.style.opacity = "0";
+
+        nav.classList.remove("nav-colored");
+        navMobile.classList.remove("nav-colored");
+    }
+}
+
+const headerObserver = new IntersectionObserver(updateNavColor, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+});
+
+headerObserver.observe(header);
+
 // Simulates a typewriter effect for text
 function typingAnim(element: HTMLElement, text: string, resolve = (value: unknown) => {return}, i = 0)
 {
