e316802f12ea6a728eacc7124ba877da35b586e8
Bruno Ancona <brunoanconasala@gmail.com>
2024-05-12 21:01:05 -0600

    Refactor scripts

--- scripts/dots.js
+++ scripts/dots.js
@@ -3,26 +3,26 @@ const body = document.body;
 const html = document.documentElement;
 const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
+// Get all dots
+const dots = document.getElementsByClassName("dot");
+
+// Sets the dots alpha based on the page's scrolling position
+function setDotsAlpha() {
+    // Calculate scroll percentage
+    const scrollPercentage = (document.documentElement.scrollTop || document.body.scrollTop)
+        / ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight);
+
+    for (let i = 0; i < 5; i++) {
+        // Find the distance between scroll percentage and dot percentile
+        const distance = (1 - Math.abs(0.25 * i - scrollPercentage));
+
+        // Set dot opacity with min 0.5
+        dots[i].style.opacity = Math.max(distance, 0.5).toString();
+    }
+}
+
 // Only use dots when page length is more than 100%
 if (height > window.innerHeight) {
-    // Get all dots
-    const dots = document.getElementsByClassName("dot");
-
-    // Sets the dots alpha based on the page's scrolling position
-    function setDotsAlpha() {
-        // Calculate scroll percentage
-        const scrollPercentage = (document.documentElement.scrollTop || document.body.scrollTop)
-            / ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight);
-
-        for (let i = 0; i < 5; i++) {
-            // Find the distance between scroll percentage and dot percentile
-            const distance = (1 - Math.abs(0.25 * i - scrollPercentage));
-
-            // Set dot opacity with min 0.5
-            dots[i].style.opacity = Math.max(distance, 0.5).toString();
-        }
-    }
-
     // Set alpha and change it on scroll event
     setDotsAlpha();
     window.addEventListener("scroll", () => setDotsAlpha(), true);
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,4 @@
 // Hamburger menu for mobile and small screens
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -15,3 +8,10 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
