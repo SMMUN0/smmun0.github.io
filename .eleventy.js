7edf054f0e8884ba0d9c6d15eacb5027f5da6295
Bruno Ancona <brunoanconasala@gmail.com>
2024-05-23 17:05:54 -0600

    Add all editions to info page

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,28 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./scripts/");
     eleventyConfig.addWatchTarget("./scripts/");
 
+    eleventyConfig.addShortcode("smmun_edition", function(year, edition, text) {
+        return `<div class="info-div" style="background-image: url('/assets/img/editions/${year}/bg.webp'); color: #fffdfd;">
+            <div class="info-title">
+                SMMUN ${year}; ${edition}
+            </div>
+            <div class="info-text">
+                ${text}
+            </div>
+            <div class="info-images">
+                <picture>
+                    <img src="/assets/img/editions/${year}/img1.webp" alt="">
+                </picture>
+                <picture>
+                    <img src="/assets/img/editions/${year}/img2.webp" alt="">
+                </picture>
+                <picture>
+                    <img src="/assets/img/editions/${year}/img3.webp" alt="">
+                </picture>
+            </div>
+        </div>`;
+    });
+
     return {
         dir: {
             input: "content",
