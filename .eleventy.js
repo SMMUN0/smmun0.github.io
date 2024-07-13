dc904646b48e1217eb7419dfe0ad43b703d21a73
Bruno Ancona <brunoanconasala@gmail.com>
2024-07-13 00:19:30 +0000

    Reorganize folder structure and minify site

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,74 @@
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier-terser");
+const { minify_sync } = require("terser");
+const path = require("path");
 const sizeOf = require("image-size");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./CNAME");
-    eleventyConfig.addPassthroughCopy("./robots.txt");
-    eleventyConfig.addPassthroughCopy({ favicon: "/" });
+    // Copy files into the output directory
+    eleventyConfig.addPassthroughCopy({"src/_passthrough": "/"});
+    eleventyConfig.addPassthroughCopy({"src/_assets": "/assets"}, {filter: path => (path.indexOf('/partners') == -1)});
 
-    eleventyConfig.addPassthroughCopy("./style/", {filter: path => (path.endsWith('alianzas.css') == false)});
-    eleventyConfig.addWatchTarget("./style/");
+    // Ignore files corresponding to unpublished "alianzas"
+    eleventyConfig.ignores.add("content/alianzas.njk");
+    eleventyConfig.ignores.add("content/style/alianzas.css");
 
-    eleventyConfig.addPassthroughCopy("./assets/", {filter: path => (path.indexOf('/partners') == -1)});
+    // Move files in 'src/content' to root of site
+    eleventyConfig.addFilter("dropContentFolder", function (path) {
+        if (path.endsWith("/index")) {
+            path = path.slice(0, -6);
+        }
 
-    eleventyConfig.addPassthroughCopy("./scripts/");
-    eleventyConfig.addWatchTarget("./scripts/");
+        const pathToDrop = "/content"
+        if (path.indexOf(pathToDrop) !== 0) {
+            return path;
+        }
 
-    eleventyConfig.ignores.add("./content/alianzas.njk");
+        return path.slice(pathToDrop.length);
+    });
 
+    // Process CSS files
+    eleventyConfig.addTemplateFormats("css");
+    eleventyConfig.addExtension("css", {
+        outputFileExtension: "css",
+        compile: async function (input) {
+            return async () => {
+                return input;
+            };
+        },
+    });
+
+    // Process JS files
+    eleventyConfig.addTemplateFormats("js");
+    eleventyConfig.addExtension("js", {
+        outputFileExtension: "js",
+        compile: async function (input) {
+            return async () => {
+                return input;
+            };
+        },
+    });
+
+    // Minify output files
+    eleventyConfig.addTransform("minify", function (content, outputPath) {
+        const extname = path.extname(outputPath);
+        switch (extname) {
+            case ".html":
+                return htmlmin.minify(content, {
+                    useShortDoctype: true,
+                    removeComments: true,
+                    collapseWhitespace: true
+                });
+            case ".css":
+                return new CleanCSS({level: 2}).minify(content).styles;
+            case ".js":
+                return minify_sync(content).code;
+            default:
+                return content;
+        }
+    });
+
+    // SMMUN editions shortcode for quienes-somos
     eleventyConfig.addShortcode("smmun_edition", function(year, edition, bgColor, text) {
         let color = "#fffdfd";
         if (year == 2016) {
@@ -25,9 +79,9 @@ module.exports = function (eleventyConfig) {
         let img2 = `assets/img/ediciones/${year}/img2.webp`;
         let img3 = `assets/img/ediciones/${year}/img3.webp`;
 
-        let img1Dimensions = sizeOf(img1);
-        let img2Dimensions = sizeOf(img2);
-        let img3Dimensions = sizeOf(img3);
+        let img1Dimensions = sizeOf(`src/_${img1}`);
+        let img2Dimensions = sizeOf(`src/_${img2}`);
+        let img3Dimensions = sizeOf(`src/_${img3}`);
 
         return `<article class="info-div" style="background-color: ${bgColor}; background-image: url('/assets/img/ediciones/${year}/bg.webp'); color:${color};">
             <h3 class="info-title">
@@ -52,9 +106,9 @@ module.exports = function (eleventyConfig) {
 
     return {
         dir: {
-            input: "content",
-            includes: "../_includes",
-            data: "../_data",
+            input: "src",
+            includes: "_includes",
+            data: "_data",
             output: "_site"
         }
     };
