a8305a60a1937c3c1b3d26b82e99f1a2bdd8a288
Bruno Ancona <brunoanconasala@gmail.com>
2025-09-15 17:55:41 -0600

    Use minifier

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,33 @@
 import adapter from "@sveltejs/adapter-static";
+import htmlMinifierAdaptor from "sveltekit-html-minifier";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: "build",
-            assets: "build",
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
+	kit: {
+		adapter: htmlMinifierAdaptor(
+			adapter({
+				pages: "build",
+				assets: "build",
+				fallback: undefined,
+				precompress: false,
+				strict: true
+			}),
+			{
+				pages: "build",
+				minifierOptions: {
+					collapseWhitespace: true,
+					collapseInlineTagWhitespace: true,
+					useShortDocType: true,
+					removeComments: true,
+					minifyCSS: true,
+					minifyJS: true
+				}
+			}
+		)
+	}
 };
 
 export default config;
