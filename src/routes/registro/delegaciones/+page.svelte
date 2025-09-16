889fc6d6fd108c9d08ee24b6ef68bb3f1e034a9c
Bruno Ancona <brunoanconasala@gmail.com>
2025-09-15 19:42:22 -0600

    Add line break in description.

--- src/routes/registro/delegaciones/+page.svelte
+++ src/routes/registro/delegaciones/+page.svelte
@@ -374,7 +374,9 @@
     <!-- Descripción -->
     <div class="mb-3">
         <p style="text-align: justify;">
-            El RLM-MUN es el <b>Modelo de Naciones Unidas de la Escuela Secundaria Ricardo López Méndez</b>. La segunda edición se llevará a cabo el <b>sábado 4 de octubre de 2025</b>, de <b>8:00 a 17:00 horas</b>, en las instalaciones de la propia secundaria, ubicada en la ciudad de <b>Valladolid, Yucatán</b>. Esta actividad está dirigida a estudiantes de entre 11 y 26 años de edad, provenientes principalmente de la zona oriente del estado, interesados en el debate, la diplomacia y el aprendizaje sobre temas internacionales.
+            El RLM-MUN es el <b>Modelo de Naciones Unidas de la Escuela Secundaria Ricardo López Méndez</b>. La segunda edición se llevará a cabo el <b>sábado 4 de octubre de 2025</b>, de <b>8:00 a 17:00 horas</b>, en las instalaciones de la propia secundaria, ubicada en la ciudad de <b>Valladolid, Yucatán</b>.
+            <br><br>
+            Esta actividad está dirigida a estudiantes de entre 11 y 26 años de edad, provenientes principalmente de la zona oriente del estado, interesados en el debate, la diplomacia y el aprendizaje sobre temas internacionales.
         </p>
     </div>
 
