7c0c443a9dc202c219192379c52793bba1496a7c
Bruno Ancona <brunoanconasala@gmail.com>
2025-09-23 19:56:44 -0600

    Actualizar comités y países

--- src/routes/registro/delegaciones/+page.svelte
+++ src/routes/registro/delegaciones/+page.svelte
@@ -22,8 +22,20 @@
             siglas: "OPS"
         },
         {
+            nombre: "Comisión Económica para América Latina y el Caribe (CEPAL)",
+            siglas: "CEPAL"
+        },
+        {
             nombre: "Organización de los Estados Americanos (OEA)",
             siglas: "OEA"
+        },
+        {
+            nombre: "Conferencia Regional sobre la Mujer de América Latina y el Caribe (CRM)",
+            siglas: "CRM"
+        },
+        {
+            nombre: "Comisión Nacional de los Derechos Humanos (CNDH)",
+            siglas: "CNDH"
         },
         {
             nombre: "Comité contra la Desaparición Forzada (CED)",
