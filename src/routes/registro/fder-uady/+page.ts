07ff5728f4b7400e5d0659ac403a83dc4422ee92
Bruno Ancona <brunoanconasala@gmail.com>
2026-02-15 20:56:39 -0600

    Add registration for FDER UADY

---
+++ src/routes/registro/fder-uady/+page.ts
@@ -0,0 +1,12 @@
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = () => {
+    return {
+        title: "Registro DO FDER UADY - SMMUN",
+        description: "",
+        navBackgroundColor: "#ffffff",
+        navTextColor: "#454140",
+        navHighlightColor: "#ea6880",
+        navHighlightTextColor: "#ffffff"
+    };
+};
